aa39e57892f39aac38dc000641ed1d4c49a3cde4
Grant Hopkins <granthopkins0315@gmail.com>
2026-05-23 14:28:06 -0500

    Imma need to sit on this. Honestly way more complicated then I was expecting, and I think it's just one of those things that needs to come in increments. Currently focusing too hard on 3-dimensional rendering when developing the 2d raycasting first on a projection plane would probably make this process much much simpilier.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ const SCREEN_WIDTH: i32 = 480;
 const MAP_WIDTH: i32 = 20;
 const MAP_HEIGHT: i32 = 20;
 
+const TILE_SIZE: i32 = 16;
+const WALL_HEIGHT: i32 = 64;
+const CAMERA_PROJECTION_HEIGHT: i32 = 32;
+
+const FOV: f64 = 60.0;
+
 #[derive(Debug)]
 struct Tile {
     blocked: bool,
@@ -26,6 +32,21 @@ impl Tile {
             block_sight: true,
         }
     }
+}
+
+struct Player {
+    px: i32,
+    py: i32,
+    view_angle: f32,
+}
+
+struct ProjectionPlane {
+    height: i32,
+    width: i32,
+    center_x: i32,
+    center_y: i32,
+    distance_from_player: f32,
+    angle_between_cols: f32,
 }
 
 type Map = Vec<Vec<Tile>>;
@@ -71,10 +92,6 @@ fn init_wall_texture(
     Ok(texture)
 }
 
-fn cast_ray_get_distance() -> f32;
-
-fn get_wall_intersection_point() -> f32;
-
 fn main() {
     // Init raylib
     let (mut rl, thread) = raylib::init()
@@ -93,50 +110,89 @@ fn main() {
     )
     .expect("Failed load wall texture");
 
+    // Initialize Player
+    let player: Player = Player {
+        px: 10,
+        py: 10,
+        view_angle: 45.0,
+    };
+
+    // Initialize Projection Plane
+    let projection_plane = ProjectionPlane {
+        height: SCREEN_HEIGHT,
+        width: SCREEN_WIDTH,
+        center_x: SCREEN_WIDTH / 2,
+        center_y: SCREEN_HEIGHT / 2,
+        distance_from_player: (SCREEN_WIDTH as f64 / (FOV.tan() / 2.0)) as f32,
+        angle_between_cols: FOV as f32 / SCREEN_WIDTH as f32,
+    };
+
     // --- GAME LOOP ---
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        for screen_x in 0..SCREEN_WIDTH {
-            // Fire ray, calc distance, and get exact intersection coordinates
-            let distance = cast_ray_get_distance();
-            let wall_hit_x: f32 = get_wall_intersection_point(); // e.g. 5.34
-
-            // Fix fish-eye
-            let corrected_distance = distance * player_relative_angle.cos();
-
-            // Calculate screen properties (Destination)
-            // Wall height gets smaller the further away it is
-            let wall_height = (SCREEN_HEIGHT as f32 / corrected_distance) as f32;
-            let screen_y = (SCREEN_HEIGHT as f32 - wall_height) / 2.0; // Center the wall
-
-            // Calculate texture coords
-            // Get the fractional part of where the wall was hit (e.g. 0.34)
-            let wall_hit_fraction = wall_hit_x - wall_hit_x.floor();
-
-            // Map that fraction to a specific 1-pixel column in the texture file
-            let texture_width = wall_tex.width as f32;
-            let texture_height = wall_tex.height as f32;
-            let tex_x = (wall_hit_fraction * texture_width) as f32;
-
-            // Define raylib rectangles
-            let source_rec = Rectangle::new(screen_x as f32, screen_y, 1.0, wall_height);
-
-            // Destination:: draw it at our current horizontal screen column, stretched vertically
-            let dest_rec = Rectangle::new(screen_x as f32, screen_y, 1.0, wall_height);
-
-            // Draw the slice
-            let origin = Vector2::new(0.0, 0.0);
-
-            // Optional: Add simple distance shadowing by tinting distant walls darker
-            let shadow_intensity = (255.0 - (corrected_distance * 10.0)).clamp(50.0, 255.0) as u8;
-            let tint = Color::new(shadow_intensity, shadow_intensity, shadow_intensity, 255);
-
-            d.draw_texture_pro(&wall_tex, source_rec, dest_rec, origin, 0.0, tint);
-        }
+        // Loop through each pixel in SCREEN_WIDTH
+        for screen_x in 0..SCREEN_WIDTH {}
 
         d.clear_background(Color::WHITE);
         d.draw_texture(&wall_tex, 10, 10, Color::WHITE);
         d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
+}
+
+// To render the scene:
+// 1. Based on the viewing angle, subtract half of FOV
+// 2. Starting from column 0:
+//      Cast a ray
+//      Trace the ray until it hits a wall
+// 3. Record the distance to the wall (the distance is qual to the length of the ray).
+// 4. Add the angle increment so that the ray moves to the right (we have this pre-determined in
+//    the ProjectPlane object)
+// 5. Repeat step 2 and 3 for each subsequent column until all rays are cast
+fn raycasting_loop(rl: &RaylibHandle, projection_plane: &ProjectionPlane, player: &Player) {
+    // Horizontal or vertical coordinate of intersection
+    let mut vertical_grid: i32;
+    let mut horizontal_grid: i32;
+
+    let mut dist_to_next_horizontal_grid: i32; // How far the next bound 
+    let mut dist_to_next_vertical_grid: i32;
+    let mut x_intersection: i32;
+    let mut y_intersection: i32;
+    let mut dist_to_next_x_intersection: i32;
+    let mut dist_to_next_y_intersection: i32;
+
+    let mut x_grid_index: i32; // The current cell the ray is in
+    let mut y_grid_index: i32;
+
+    let mut dist_to_vertical_grid_being_hit: i32; // The distance of the x and y ray intersection
+    // from the viewport
+    let mut dist_to_horizontal_grid_being_hit: i32;
+
+    // FOV is defined, so half would be players direction in the middle
+    // We will trace the rays starting from the leftmost ray
+
+    // Ray is between 0 to 180 degress (1st and 2nd quadrant).
+    let mut cast_arc: i32 = player.view_angle as i32 / FOV as i32;
+
+    // Wrap around if nessacary
+    if cast_arc < 0 {
+        cast_arc += 360;
+    }
+
+    for screen_x in 0..SCREEN_WIDTH {
+        //  Ray is between 0 and 180 degree (1st and 2nd quadrant)
+
+        // Ray is facing down
+        if (cast_arc > 0 && cast_arc < 180) {
+            // Truncate then add to get the cooridnate of the FIRST grid (horizontal wall) this is
+            // in front of the player (this is in pixel unit)
+            // ROUNDED DOWN
+            horizontal_grid = (player.py / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
+
+            // Compute distance to the next horizontal wall
+            dist_to_next_horizontal_grid = TILE_SIZE;
+
+            let mut xtemp = 
+        }
     }
 }
